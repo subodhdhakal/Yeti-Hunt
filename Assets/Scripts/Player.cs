65e33f116250e1aeca3661a04077d15b718b6d7f
Subodh Dhakal <subodh.dhakal@mavs.uta.edu>
2019-03-23 21:48:10 -0500

    Sprite Character Animation Idle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainPlayer : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("im alive");
+        Rigidbody _rig = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame

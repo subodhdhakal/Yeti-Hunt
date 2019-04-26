0d14c2b8d225fe2c1ec71aaf9a8b0bfee7dce630
Clay Cardenas <ogclaystick@gmail.com>
2019-04-25 19:40:26 -0500

    Removed Yeti Rage Mode
    Still under work

---
+++ Assets/RemoveComponentNameEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveComponentName : MonoBehaviour
+{
+
+    public void RemoveComponents()
+    {
+        Component[] components = GetComponentsInChildren(typeof(RegisterRage), true);
+
+        foreach (var c in components)
+        {
+            DestroyImmediate(c);
+        }
+    }
+}
